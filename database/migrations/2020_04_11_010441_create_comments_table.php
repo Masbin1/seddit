1d654a2180f7739bfc1a2d52d9258b50b83e06cc
Masbin1 <83834217+Masbin1@users.noreply.github.com>
2022-01-31 10:39:07 +0700

    oke

--- database/migrations/2020_04_11_010441_create_comments_table.php
+++ database/migrations/2020_04_11_010441_create_comments_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateCommentsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations
      *
      * @return void
      */
